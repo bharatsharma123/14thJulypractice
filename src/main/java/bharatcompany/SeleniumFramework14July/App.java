0ea1e4466968a071d6d59deec7c0e1df44ea025c
bharatsharma123 <mebharatbhardwaj@gmail.com>
2025-08-21 10:51:33 +0530

    First commit

---
+++ src/main/java/bharatcompany/SeleniumFramework14July/App.java
@@ -0,0 +1,10 @@
+package bharatcompany.SeleniumFramework14July;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
